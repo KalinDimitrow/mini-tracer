19db11af0f733cae220259bcea6267b5ff47d3d2
KalinDimitrow <kalindimitrovv@gmail.com>
2021-02-25 22:49:39 +0200

    remove bug where camera is not symetric

--- src/scene/camera.rs
+++ src/scene/camera.rs
@@ -34,8 +34,8 @@ impl Camera {
         let top_left = rotation * top_left;
         let top_right = rotation * top_right;
         let bottom_left = rotation * bottom_left;
-        let right_direction = (top_right - top_left).normalize();
-        let down_direction = (bottom_left - top_left).normalize();
+        let right_direction = (top_right - top_left);
+        let down_direction = (bottom_left - top_left);
         Camera {position, pitch, yaw, row, aspect_ratio, fov, top_left, right_direction, down_direction, resolution}
     }
 
