64c2112f59ded198fbbf2f9a098bf62bbb22bfff
KalinDimitrow <kalindimitrovv@gmail.com>
2021-05-05 20:29:10 +0300

    #7 update dependencies

--- src/auxiliary/texture.rs
+++ src/auxiliary/texture.rs
@@ -1,3 +1,5 @@
+use std::num::NonZeroU32;
+
 use anyhow::*;
 use image::GenericImageView;
 
@@ -31,7 +33,7 @@ impl HardwareTexture {
         let size = wgpu::Extent3d {
             width: dimensions.0,
             height: dimensions.1,
-            depth: 1,
+            depth_or_array_layers: 1,
         };
         let texture = device.create_texture(&wgpu::TextureDescriptor {
             label,
@@ -44,16 +46,16 @@ impl HardwareTexture {
         });
 
         queue.write_texture(
-            wgpu::TextureCopyView {
+            wgpu::ImageCopyTexture {
                 texture: &texture,
                 mip_level: 0,
                 origin: wgpu::Origin3d::ZERO,
             },
             data,
-            wgpu::TextureDataLayout {
+            wgpu::ImageDataLayout {
                 offset: 0,
-                bytes_per_row: 4 * dimensions.0,
-                rows_per_image: dimensions.1,
+                bytes_per_row: NonZeroU32::new(4 * dimensions.0),
+                rows_per_image: NonZeroU32::new(dimensions.1),
             },
             size,
         );
--- src/renderers/rendering_context.rs
+++ src/renderers/rendering_context.rs
@@ -1,5 +1,6 @@
 use std::iter;
 use wgpu::util::DeviceExt;
+use wgpu_types::*;
 use winit::{
     event::*,
     window::{Window},
@@ -26,21 +27,21 @@ struct Vertex {
 }
 
 impl Vertex {
-    fn desc<'a>() -> wgpu::VertexBufferDescriptor<'a> {
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
         use std::mem;
-        wgpu::VertexBufferDescriptor {
-            stride: mem::size_of::<Vertex>() as wgpu::BufferAddress,
+        wgpu::VertexBufferLayout {
+            array_stride: mem::size_of::<Vertex>() as wgpu::BufferAddress,
             step_mode: wgpu::InputStepMode::Vertex,
             attributes: &[
-                wgpu::VertexAttributeDescriptor {
+                wgpu::VertexAttribute {
                     offset: 0,
                     shader_location: 0,
-                    format: wgpu::VertexFormat::Float3,
+                    format: wgpu::VertexFormat::Float32x3,
                 },
-                wgpu::VertexAttributeDescriptor {
+                wgpu::VertexAttribute {
                     offset: mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
                     shader_location: 1,
-                    format: wgpu::VertexFormat::Float2,
+                    format: wgpu::VertexFormat::Float32x2,
                 },
             ],
         }
@@ -66,7 +67,7 @@ const VERTICES: &[Vertex] = &[
     }, // D
 ];
 
-const INDICES: &[u16] = &[0, 1, 3, 1, 2, 3];
+const INDICES: &[u16] = &[0, 1, 2, 0, 2, 3];
 
 pub struct RenderingContext {
     pub surface: wgpu::Surface,
@@ -91,7 +92,7 @@ impl RenderingContext {
         use futures::executor::block_on;
         let (device, queue, surface) = block_on(RenderingContext::instantiate(window))?;
         let sc_desc = wgpu::SwapChainDescriptor {
-            usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+            usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
             format: wgpu::TextureFormat::Bgra8UnormSrgb,
             width: size.width,
             height: size.height,
@@ -109,17 +110,17 @@ impl RenderingContext {
                     wgpu::BindGroupLayoutEntry {
                         binding: 0,
                         visibility: wgpu::ShaderStage::FRAGMENT,
-                        ty: wgpu::BindingType::SampledTexture {
+                        ty: wgpu::BindingType::Texture {
+                            sample_type: wgpu::TextureSampleType::Float{filterable: false},
+                            view_dimension: wgpu::TextureViewDimension::D2,
                             multisampled: false,
-                            dimension: wgpu::TextureViewDimension::D2,
-                            component_type: wgpu::TextureComponentType::Uint,
                         },
                         count: None,
                     },
                     wgpu::BindGroupLayoutEntry {
                         binding: 1,
                         visibility: wgpu::ShaderStage::FRAGMENT,
-                        ty: wgpu::BindingType::Sampler { comparison: false },
+                        ty: wgpu::BindingType::Sampler { filtering: true, comparison: false },
                         count: None,
                     },
                 ],
@@ -141,8 +142,8 @@ impl RenderingContext {
             label: Some("diffuse_bind_group"),
         });
 
-        let vs_module = device.create_shader_module(wgpu::include_spirv!("shaders/shader.vert.spv"));
-        let fs_module = device.create_shader_module(wgpu::include_spirv!("shaders/shader.frag.spv"));
+        let vs_module = device.create_shader_module(&wgpu::include_spirv!("shaders/shader.vert.spv"));
+        let fs_module = device.create_shader_module(&wgpu::include_spirv!("shaders/shader.frag.spv"));
 
         let render_pipeline_layout =
             device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
@@ -154,37 +155,33 @@ impl RenderingContext {
         let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label: Some("Render Pipeline"),
             layout: Some(&render_pipeline_layout),
-            vertex_stage: wgpu::ProgrammableStageDescriptor {
+            vertex: wgpu::VertexState {
                 module: &vs_module,
                 entry_point: "main",
+                buffers : &[Vertex::desc()]
             },
-            fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
+
+            primitive : wgpu::PrimitiveState {
+                topology : PrimitiveTopology::TriangleStrip,
+                // ..Default::default()
+                strip_index_format : None,//Some(wgpu::IndexFormat::Uint16),
+                front_face : wgpu::FrontFace::Cw,
+                cull_mode : Some(wgpu::Face::Back),
+                clamp_depth : false,
+                polygon_mode : wgpu::PolygonMode::Fill,
+                conservative : false
+            },
+
+            depth_stencil : None,
+
+            multisample : MultisampleState::default(),
+
+            fragment: Some(wgpu::FragmentState {
                 module: &fs_module,
                 entry_point: "main",
+                targets: &[wgpu::TextureFormat::Bgra8UnormSrgb.into()]
             }),
-            rasterization_state: Some(wgpu::RasterizationStateDescriptor {
-                front_face: wgpu::FrontFace::Ccw,
-                cull_mode: wgpu::CullMode::Back,
-                depth_bias: 0,
-                depth_bias_slope_scale: 0.0,
-                depth_bias_clamp: 0.0,
-                clamp_depth: false,
-            }),
-            primitive_topology: wgpu::PrimitiveTopology::TriangleList,
-            color_states: &[wgpu::ColorStateDescriptor {
-                format: sc_desc.format,
-                color_blend: wgpu::BlendDescriptor::REPLACE,
-                alpha_blend: wgpu::BlendDescriptor::REPLACE,
-                write_mask: wgpu::ColorWrite::ALL,
-            }],
-            depth_stencil_state: None,
-            vertex_state: wgpu::VertexStateDescriptor {
-                index_format: wgpu::IndexFormat::Uint16,
-                vertex_buffers: &[Vertex::desc()],
-            },
-            sample_count: 1,
-            sample_mask: !0,
-            alpha_to_coverage_enabled: false,
+
         });
 
         let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
@@ -223,7 +220,7 @@ impl RenderingContext {
         let surface = unsafe { instance.create_surface(window) };
         let adapter = instance
             .request_adapter(&wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::Default,
+                power_preference: wgpu::PowerPreference::HighPerformance,
                 compatible_surface: Some(&surface),
             })
             .await.ok_or(NoDisplayAdaptorError{})?;
@@ -231,14 +228,15 @@ impl RenderingContext {
         let (device, queue) = adapter
             .request_device(
                 &wgpu::DeviceDescriptor {
+                    label: Some("DeviceDescriptor"),
                     features: wgpu::Features::empty(),
-                    limits: wgpu::Limits::default(),
-                    shader_validation: true,
+                    limits: Limits::default(),
                 },
                 None, // Trace path
             )
             .await?;
 
+        // let swapchain_format = adapter.get_swap_chain_preferred_format(&surface).unwrap();
         Ok((device, queue, surface))
     }
 
@@ -278,18 +276,18 @@ impl RenderingContext {
                         wgpu::BindGroupLayoutEntry {
                             binding: 0,
                             visibility: wgpu::ShaderStage::FRAGMENT,
-                            ty: wgpu::BindingType::SampledTexture {
+                            ty: wgpu::BindingType::Texture {
+                                sample_type: wgpu::TextureSampleType::Float{filterable: false},
+                                view_dimension: wgpu::TextureViewDimension::D2,
                                 multisampled: false,
-                                dimension: wgpu::TextureViewDimension::D2,
-                                component_type: wgpu::TextureComponentType::Uint,
                             },
                             count: None,
                         },
                         wgpu::BindGroupLayoutEntry {
                             binding: 1,
                             visibility: wgpu::ShaderStage::FRAGMENT,
-                            ty: wgpu::BindingType::Sampler { comparison: false },
-                            count: None,
+                            ty: wgpu::BindingType::Sampler { filtering: true, comparison: false },
+                            count:  None,//std::num::NonZeroU32::new(1),
                         },
                     ],
                     label: Some("texture_bind_group_layout"),
@@ -314,8 +312,9 @@ impl RenderingContext {
 
         {
             let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
-                    attachment: &frame.view,
+                label : Some("Render pass"),
+                color_attachments: &[wgpu::RenderPassColorAttachment {
+                    view: &frame.view,
                     resolve_target: None,
                     ops: wgpu::Operations {
                         load: wgpu::LoadOp::Clear(wgpu::Color {
@@ -333,7 +332,7 @@ impl RenderingContext {
             render_pass.set_pipeline(&self.render_pipeline);
             render_pass.set_bind_group(0, &self.cached_diffuse_bind_group, &[]);
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.set_index_buffer(self.index_buffer.slice(..));
+            render_pass.set_index_buffer(self.index_buffer.slice(..), IndexFormat::Uint16);
             render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
